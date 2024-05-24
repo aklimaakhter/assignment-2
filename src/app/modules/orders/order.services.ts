b74b4f3c17dcca7ff2d0a96d41bd13f1a065e3be
aklimaakhter <aklima00191@gmail.com>
2024-05-25 02:54:03 +0600

    README.md file added

--- src/app/modules/orders/order.services.ts
+++ src/app/modules/orders/order.services.ts
@@ -1,7 +1,26 @@
+import { ProductModel } from '../product/product.model'
 import { Order } from './order.interface'
 import { OrderModel } from './order.model'
 
 const createOrderIntoDB = async (order: Order) => {
+  // const result = await OrderModel.create(order)
+  // return result
+  const product = await ProductModel.findById(order.productId)
+
+  console.log('my pp', product)
+
+  if (!product) {
+    throw Error('no product found')
+  }
+
+  if (order.quantity > product.inventory.quantity) {
+    throw Error('Insufficient quantity available in inventory')
+  }
+
+  product.inventory.quantity -= order.quantity
+  product.inventory.inStock = product.inventory.quantity > 0
+
+  await product.save()
   const result = await OrderModel.create(order)
   return result
 }
